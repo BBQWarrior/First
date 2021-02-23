696f7fa20d6ca03c0e245fd8fa0c8cc8da04c5c6
Zsolt <zsolt.mar02@gmail.com>
2021-02-23 19:55:53 +0200

    sum of a and b

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
+int sum(int *a, int *b);
+
 int main() {
-    printf("Hello, World!\n");
+    int a, b;
+
+    scanf("%d%d", &a, &b);
+
+    printf("%d", sum(&a,&b));
     return 0;
+}
+
+int sum(int *a, int *b){
+    int sumAB;
+
+    sumAB = *a + *b;
+
+    return sumAB;
 }
